51e109c3295cd5917dc5c8719649db7aa9433f7d
abhipaiangle <abhishekangle6gmail.com>
2020-07-14 02:30:17 +0530

    Initial commit

---
+++ data_gen.py
@@ -0,0 +1,16 @@
+
+# Generate dataset entries and run the below commented part to add entry to csv file
+
+'''
+X=[] # Add 20 values
+y = # Add flag
+entry = X
+entry.append(y)
+# First 20 columns are points and 21st column is flag
+import csv
+with open('dataset.csv','a',newline='') as file:
+    wr = csv.writer(file,dialect='excel')
+    wr.writerow(entry)
+'''    
+    
+    
