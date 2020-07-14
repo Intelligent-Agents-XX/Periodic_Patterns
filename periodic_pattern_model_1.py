fa7942236348228420da5ee7bbcc6b5633cf2033
abhipaiangle <abhishekangle6gmail.com>
2020-07-14 18:36:11 +0530

    Model 1 added

---
+++ periodic_pattern_model_1.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+from sklearn.externals import joblib 
+
+# make sure you have downloaded the periodic_pattern_model_1.pkl file and is present in the current directory 
+
+classifier = joblib.load('periodic_pattern_model_1.pkl')
+
+test = np.array([1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6,1,2]).reshape(1,-1) # Add 20 values to the array and execute
+
+IsItPattern = classifier.predict(test) > 0.5 # Remove > 0.5 if you want to know how good the pattern is
+
+print(IsItPattern)
