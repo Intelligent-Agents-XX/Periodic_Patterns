58beb19f8a0d7f393e141ddc31c60d00fb1e7b49
abhipaiangle <abhishekangle6gmail.com>
2020-07-15 04:51:46 +0530

    model 2 added

--- periodic_pattern_model.py
+++ periodic_pattern_model.py
@@ -4,9 +4,9 @@ from sklearn.externals import joblib
 
 # make sure you have downloaded the periodic_pattern_model_1.pkl file and is present in the current directory 
 
-classifier = joblib.load('periodic_pattern_model_1.pkl')
+classifier = joblib.load('periodic_pattern_model_2.pkl')
 
-test = np.array([1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6,1,2]).reshape(1,-1) # Add 20 values to the array and execute
+test = np.array([1,2,4,7,4,2,1,2,4,7,4,2,1,2,4,7,4,2,1,2]).reshape(1,-1) # Add 20 values to the array and execute
 
 IsItPattern = classifier.predict(test) > 0.5 # Remove > 0.5 if you want to know how good the pattern is
 
--- periodic_pattern_model_2_structure.py
+++ periodic_pattern_model_2_structure.py
@@ -15,9 +15,9 @@ from keras.layers import Dense
 
 classifier = Sequential()
 
-classifier.add(Dense(output_dim = 100, init="uniform", activation = "relu",  input_dim = 20))
+classifier.add(Dense(output_dim = 80, init="uniform", activation = "relu",  input_dim = 20))
 
-classifier.add(Dense(output_dim = 100, init="uniform", activation = "relu"))
+classifier.add(Dense(output_dim = 400, init="uniform", activation = "relu"))
 
 classifier.add(Dense(output_dim = 1, init="uniform", activation = "sigmoid"))
 
@@ -34,3 +34,10 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test,y_pred)
 
 accuracy = accuracy_score(y_test,y_pred)
+
+'''
+from sklearn.externals import joblib 
+  
+# Save the model as a pickle in a file 
+joblib.dump(classifier, 'periodic_pattern_model_x.pkl')
+'''
